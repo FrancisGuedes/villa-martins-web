55b2f6a30c8617a650e9f8a8ce5756e6706d15c8
FranciscoGuedes <francisco.guedes@polarising.com>
2022-10-06 00:45:25 +0100

    some initial updates

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Test.module.scss'
+
+export type Ad = {
+  lalal: [];
+}
+
+const Home: NextPage = () => {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectet?
+    </>
+  )
+}
+
+export default Home;

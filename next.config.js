f5c9411ec907b57654e88b372c08809fdcbad79b
FranciscoGuedes <francisco.guedes@polarising.com>
2022-10-31 12:56:31 +0000

    new configs to use svg

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
   /* sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   }, */

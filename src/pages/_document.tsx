699f666ec9b7cd0b1f27628c5e6f1eed8d4df0c7
FranciscoGuedes <francisco.guedes@polarising.com>
2022-11-16 22:36:48 +0000

    updated index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,15 @@ class MyDocument extends Document {
     return (
       <Html lang={MyDocument.labels.lang}>
         <Head />
+        <meta charSet={MyDocument.labels.charSet} />
+        <meta
+          name={MyDocument.labels.name}
+          content={MyDocument.labels.content}
+          />
+        <link 
+          rel={MyDocument.labels.rel} 
+          href={MyDocument.labels.href} 
+        />
         <title>{MyDocument.labels.title}</title>
         <body>
           <Main />

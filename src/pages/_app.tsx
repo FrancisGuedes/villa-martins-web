db9bf7eca8bb72535cd928ccfd3b13b983f32489
Francisco Guedes <franciscojmguedes@gmaiil.com>
2025-02-23 18:04:26 +0000

    add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  const HOTJAR_ID = process.env.NEXT_PUBLIC_HOTJAR_ID
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
@@ -53,6 +54,25 @@ function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+
+      {/* Hotjar Script */}
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+
       <Component {...pageProps} />
       <Analytics />
     </>
